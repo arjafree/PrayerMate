528ff894269187b12f4b7df4ec66e31e19d429f8
arjafree <107591831+arjafree@users.noreply.github.com>
2023-07-30 07:54:14 +0800

    Initial Commit

---
+++ PrayerMate2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PrayerMate2
+//
+//  Created by Aliraza Jafree on 30/7/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrayerMate2/PrayerMate2App.swift
@@ -0,0 +1,17 @@
+//
+//  PrayerMate2App.swift
+//  PrayerMate2
+//
+//  Created by Aliraza Jafree on 30/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrayerMate2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
